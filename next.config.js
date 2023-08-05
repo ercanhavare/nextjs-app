e46a2d13e028645ca50457666459d36a0c4596b2
ercanhavare <ercanhavare@gmail.com>
2023-08-05 12:36:10 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // for import external img
+    images:{
+        domains:[
+            "images.pexels.com",
+            "example.com",
+        ]
+    }
+}
 
 module.exports = nextConfig
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,21 @@
+//"use client"
+import React from "react";
+import styles from './page.module.css';
+import Image from "next/image";
+
+const Footer = () => {
+
+    return (
+        <div className={styles.container}>
+            <div>@2023 Oktay. All rights reserved</div>
+            <div className={styles.social}>
+                <Image src="/1.png" width={15} height={15} className={styles.icon} alt="nexT Project"/>
+                <Image src="/2.png" width={15} height={15} className={styles.icon} alt="nexT Project"/>
+                <Image src="/3.png" width={15} height={15} className={styles.icon} alt="nexT Project"/>
+                <Image src="/4.png" width={15} height={15} className={styles.icon} alt="nexT Project"/>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
