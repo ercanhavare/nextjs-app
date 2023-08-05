f407bd21f02c19050dcd660e5039da70439f2edb
ercanhavare <ercanhavare@gmail.com>
2023-08-05 14:42:40 +0300

    fetch data

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
+import { notFound } from "next/navigation";
+
+async function getData({id}){
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+     // next:{ revalidate:10}
+     cache:"no-store"
+  });
+
+  if(!res.ok){
+  //    throw new Error('Failed to fetch data');
+      notFound()
+    }
+
+  return res.json();
+}
 
 
-const BlogPost = () => {
+const BlogPost = async ({params}) => {
+
+  const data = await getData(params.id)
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>Lorem ipsum</h1>
           <p className={styles.desc}>
-           Lorem ipsum
+         {data.title}
           </p>
           <div className={styles.author}>
             <Image
-                src="https://images.pexels.com/photos/16353919/"
+                src="https://images.pexels.com/photos/3635300/pexels-photo-3635300.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt=""
               width={40}
               height={40}
@@ -25,7 +43,7 @@ const BlogPost = () => {
         </div>
         <div className={styles.imageContainer}>
           <Image
-              src="https://images.pexels.com/photos/16353919/"
+              src="https://images.pexels.com/photos/3635300/pexels-photo-3635300.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
             fill={true}
             className={styles.image}
